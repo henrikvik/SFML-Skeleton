c5d3c3dc4fd38f2428b97e926071b04e1c8e682d
Henrik Vik <henkvik@gmail.com>
2018-02-01 10:10:15 +0100

    boiler plate done

---
+++ SFML-Skeleton/main.cpp
@@ -0,0 +1,29 @@
+#include <Windows.h>
+#include <SFML/Graphics.hpp>
+
+#if _DEBUG
+int main()
+#else
+INT WinMain(HINSTANCE,HINSTANCE,PSTR, INT)
+#endif
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
